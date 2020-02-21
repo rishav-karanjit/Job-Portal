b9ae5ca36ddc4fb86fec18b378ee131cea84c29b
Rishav Karanjit <karanjitrishav4@gmail.com>
2020-02-21 15:57:04 +0530

    Updated name of url path in jobseeker

--- jobseekerapp/url.py
+++ jobseekerapp/url.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/',views.DashboardView.as_view(),name="dashboard"),
-    path('viewvacancy/',views.ViewVacancy.as_view(),name="ViewVacancy"),   
+    path('dashboard/',views.DashboardView.as_view(),name="Jdashboard"),
+    path('viewvacancy/',views.ViewVacancy.as_view(),name="JViewVacancy"),   
 ]

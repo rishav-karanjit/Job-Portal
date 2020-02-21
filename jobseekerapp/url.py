58ed17eca21860f9fb39d865465252836edde643
Rishav Karanjit <karanjitrishav4@gmail.com>
2020-02-21 16:33:07 +0530

    added viewvacancy backend(jseeker)

--- jobseekerapp/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard/',views.DashboardView.as_view(),name="Jdashboard"),
-    path('viewvacancy/',views.ViewVacancy.as_view(),name="JViewVacancy"),   
-]
--- jobseekerapp/views.py
+++ jobseekerapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from django.views import generic
+from recruiterapp.models import *
 # Create your views here.
-class DashboardView():
-    pass
+class DashboardView(generic.TemplateView):
+    template_name='Jobseeker/dashboard.html'
 class ViewVacancy(generic.ListView):
     model= Vacancy
     template_name='Jobseeker/ViewVacancy.html'
     context_object_name='vacancys'
+    def get_queryset(self):
+        return Vacancy.objects.all()
